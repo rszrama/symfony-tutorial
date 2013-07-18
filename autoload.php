15256c1dd59f66df5c94bce40309eab13bfe7a47
rszrama <ryan.szrama@gmail.com>
2013-07-17 22:53:36 -0400

    Adding the Part 1 code and removing the readme from the tutorial part branches.

---
+++ autoload.php
@@ -0,0 +1,11 @@
+<?php
+ 
+// framework/autoload.php
+ 
+require_once __DIR__.'/vendor/symfony/class-loader/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+ 
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+ 
+$loader = new UniversalClassLoader();
+$loader->register();
+
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+ 
+$input = $_GET['name'];
+ 
+printf('Hello %s', $input);
