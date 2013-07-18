dd1ab42bd62f96eac87df3dfc17fe4854e7f38d2
rszrama <ryan.szrama@gmail.com>
2013-07-17 23:43:19 -0400

    Update to the code finalized in Part 2 including the call to Response::prepare.

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
 // framework/index.php
- 
-$input = $_GET['name'];
- 
-printf('Hello %s', $input);
+
+require_once __DIR__.'/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->prepare($request);
+$response->send();
