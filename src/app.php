546071e6de934169c6bf21acaf8cef74bc32cb12
rszrama <ryan.szrama@gmail.com>
2013-07-19 00:00:40 -0400

    Introduce the simple render_template() route controller.

--- src/app.php
+++ src/app.php
@@ -5,7 +5,12 @@
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'World')));
-$routes->add('bye', new Routing\Route('/bye'));
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+    'name' => 'World',
+    '_controller' => 'render_template',
+)));
+$routes->add('bye', new Routing\Route('/bye', array(
+    '_controller' => 'render_template',
+)));
 
 return $routes;
--- web/front.php
+++ web/front.php
@@ -10,6 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function render_template($request)
+{
+    extract($request->attributes->all(), EXTR_SKIP);
+    ob_start();
+    include sprintf(FRAMEWORK_ROOT.'/src/pages/%s.php', $_route);
+
+    return new Response(ob_get_clean());
+}
+
 $request = Request::createFromGlobals();
 $routes = include FRAMEWORK_ROOT.'/src/app.php';
 
@@ -18,11 +27,8 @@ $context->fromRequest($request);
 $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 
 try {
-    extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-    ob_start();
-    include sprintf(FRAMEWORK_ROOT.'/src/pages/%s.php', $_route);
-
-    $response = new Response(ob_get_clean());
+    $request->attributes->add($matcher->match($request->getPathInfo()));
+    $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
     $response = new Response('Not Found', 404);
 } catch (Exception $e) {
