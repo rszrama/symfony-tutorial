5ad820dec6508f7334e548dc45fffc1a8c090262
rszrama <ryan.szrama@gmail.com>
2013-07-19 14:36:14 -0400

    Switch to using a LeapYearController class, but simply instantiate it when adding the route.

--- src/app.php
+++ src/app.php
@@ -13,16 +13,22 @@ function is_leap_year($year = null) {
     return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
 }
 
-$routes = new Routing\RouteCollection();
-$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
-    'year' => null,
-    '_controller' => function ($request) {
+class LeapYearController
+{
+    public function indexAction($request)
+    {
         if (is_leap_year($request->attributes->get('year'))) {
             return new Response('Yep, this is a leap year!');
         }
 
         return new Response('Nope, this is not a leap year.');
     }
+}
+
+$routes = new Routing\RouteCollection();
+$routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
+    'year' => null,
+    '_controller' => array(new LeapYearController(), 'indexAction'),
 )));
 
 return $routes;
